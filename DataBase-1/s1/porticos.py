a3cc0ca9c2596608f82bb6f5708b0f1b4ad92154
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-03-29 09:32:03 -0500

    pandas db

--- DataBase-1/s1/porticos.py
+++ DataBase-1/s1/porticos.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 df = pd.read_csv("Porticos.csv", sep=";")
 
-print(df.head())
-
 # Pregunta 1
 
 # categoria = df.groupby(["CATEGORIA"]).count()
@@ -12,23 +10,49 @@ print(df.head())
 #
 # print(max(categoria["ID"]))
 
+# La categoria con mayor cantidad de vehículos es N1
+
+
 # Pregunta 2
 # filtro = df["CATEGORIA"] == "N1"
 #
 # N1 = df[filtro]
 #
-# N1.["FECHA"] = pd.to_datetime(N1["FECHA"])
+# N1["FECHA"] = pd.to_datetime(N1["FECHA"])
 #
 # print(N1.FECHA.dt.month.value_counts())
 
+# Noviembre
+
+
 # Pregunta 3
-frec = df[["FRECUENCIA"]].value_counts()
 
-# print(frec)
-
-filter1 = df[df["FRECUENCIA"] == 23]
-filter2 = df[df["FRECUENCIA"] == 22]
-
+# frec = df[["FRECUENCIA"]].value_counts()
+#
+# # print(frec)
+#
+# filter1 = df[df["FRECUENCIA"] == 23]
+# filter2 = df[df["FRECUENCIA"] == 22]
+#
 # print(filter1[["PLACA"]])
+# print(filter2[["PLACA"]])
+
+# Max cantidad de veces que pasó por el pórtico = 23
 
 
+# Pregunta 4
+#
+# N3 = df[(df["CATEGORIA"] == "N3") & (df["ALERTA"] == 2111)]
+#
+# N3["FECHA"] = pd.to_datetime(N3["FECHA"])
+#
+# print(N3.FECHA.dt.month.value_counts())
+
+# Noviembre
+
+
+# Pregunta 5
+
+# alertas = df[df[""]]
+
+print(df.ALERTA.value_counts())
