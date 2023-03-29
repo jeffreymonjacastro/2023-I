34a2c4234c850b2bed5d45525e6c140611cbe3b4
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-03-29 09:08:38 -0500

    avance

--- DataBase-1/s1/porticos.py
+++ DataBase-1/s1/porticos.py
@@ -1,12 +1,34 @@
 import pandas as pd
 
+df = pd.read_csv("Porticos.csv", sep=";")
+
+print(df.head())
+
 # Pregunta 1
-database = pd.read_csv("Porticos.csv", sep=";")
 
-categoria = database.groupby(["CATEGORIA"]).count()
+# categoria = df.groupby(["CATEGORIA"]).count()
+#
+# print(categoria["ID"])
+#
+# print(max(categoria["ID"]))
 
-print(categoria["ID"])
+# Pregunta 2
+# filtro = df["CATEGORIA"] == "N1"
+#
+# N1 = df[filtro]
+#
+# N1.["FECHA"] = pd.to_datetime(N1["FECHA"])
+#
+# print(N1.FECHA.dt.month.value_counts())
 
-print(max(categoria["ID"]))
+# Pregunta 3
+frec = df[["FRECUENCIA"]].value_counts()
+
+# print(frec)
+
+filter1 = df[df["FRECUENCIA"] == 23]
+filter2 = df[df["FRECUENCIA"] == 22]
+
+# print(filter1[["PLACA"]])
 
 
