fcb30d0f8d52fa12d0d4018bc3e24fe94a985c25
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-11 15:44:08 -0500

    Avance dbp

--- DBP/S2/scripts.js
+++ DBP/S2/scripts.js
@@ -3,14 +3,39 @@ var stature = document.getElementById("talla");
 var weight = document.getElementById("peso");
 var button = document.getElementById("button");
 var result = document.getElementById("result");
+var alerta = document.getElementById("alert");
 
 button.addEventListener('click', () => {
     let imc
     let talla = parseFloat(stature.value)
     let peso = parseFloat(weight.value)
-
-    console.log("Hola");
+    let text
     
+    if (nombre.value == "") {
+        alerta.style.display = "flex"
+        alerta.textContent = "Write your name"
+    } else {
+        alerta.style.display = "none"
+        alerta.textContent = ""
+    }
+
+    if (stature.value == "") {
+        alerta.style.display = "flex"
+        alerta.textContent += " Put your stature"
+    } else {
+        alerta.style.display = "none"
+        alerta.textContent = ""
+    }
+
+    if (weight.value == "") {
+        alerta.style.display = "flex"
+        alerta.textContent += " Put your weight"
+    } else {
+        alerta.style.display = "none"
+        alerta.textContent = ""
+    }
+    
+
     if (talla > 100) {
         imc = peso/(Math.pow(talla/100, 2))
         
@@ -18,6 +43,13 @@ button.addEventListener('click', () => {
         imc = peso/(talla*talla)
     }
 
+    if (imc < 18.5) {
+        text = "Low"
+    } else if (imc >= 18.5 && imc < 24.9) {
+        text = "Normal"
+    }
+
     result.style.display = "flex"
-    result.textContent = `Hello ${nombre.value}, your IMC is ${imc.toFixed(2)}`
+    result.textContent = `Hello ${nombre.value}, your IMC is ${imc.toFixed(2)}. You have a ${text} IMC`
+
 });
