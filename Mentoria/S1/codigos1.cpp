86cedbed9cd9a0f8a2214da39f9d6410f5760a30
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-10 19:05:35 -0500

    Some codes

---
+++ Mentoria/S1/codigos1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+void noretorno(){
+    cout<<"hola"<<endl;
+}
+
+int entero(int a, int b){
+    int sum;
+    sum = a +b;
+    return sum;
+}
+
+long long int hello(){
+    int n = 5;
+
+    return n;
+}
+
+string cadena(){
+
+    return "Me llamo c++";
+}
+
+char caracter(){
+
+    return 'a';
+}
+
+
+
+
+int main(){
+//    Ctrl + /
+//    int n, m = 0;
+//
+//    cout<<"Ingrese el num: "<<endl;
+//    cin>>n;
+//
+//    m += (n%10)*100;
+//    n /= 10;
+//    m += (n%10)*10;
+//    n /= 10;
+//    m += n;
+//
+//    cout<<m<<endl;
+
+//    int s1, s2, s3;
+//    cout<<"Ingrese los lados: "<<endl;
+//    cin>>s1>>s2>>s3;
+//
+//    if(s1 + s2 > s3 && s2 + s3 > s1 && s1 + s3 > s2)
+//        cout<<"Es triangulo"<<endl;
+//    else
+//        cout<<"No es triangulo"<<endl;
+
+
+    return 0;
+}
