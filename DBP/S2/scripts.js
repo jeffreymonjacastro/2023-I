cf48141929368577434de89607183be8963cb79f
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-13 16:06:23 -0500

    Finishhh RGB

--- DBP/S2/scripts.js
+++ DBP/S2/scripts.js
@@ -4,6 +4,7 @@ var weight = document.getElementById("peso");
 var button = document.getElementById("button");
 var result = document.getElementById("result");
 var alerta = document.getElementById("alert");
+var container = document.getElementById("container");
 
 
 button.addEventListener('click', () => {
@@ -48,6 +49,7 @@ button.addEventListener('click', () => {
 
     if (bool) {
         alerta.style.display = "none"
+        alertas = []
 
         if (talla > 2.2) {
             imc = peso/(Math.pow(talla/100, 2))
@@ -58,18 +60,24 @@ button.addEventListener('click', () => {
     
         if (imc < 18.5) {
             text = "under weight, consider eating more to regulate your weight."
+            container.style.background = "linear-gradient(90deg, rgba(0,255,229,1) 0%, rgba(48,191,253,1) 65%, rgba(69,86,252,1) 100%)"
         } else if (imc >= 18.5 && imc < 24.9) {
             text = "a Healthy weight, excellent! :D"
+            container.style.background = "linear-gradient(90deg, rgba(71,255,0,1) 0%, rgba(44,255,78,1) 65%, rgba(19,172,0,1) 100%)"
         } else if (imc >= 24.9 && imc < 29.9) {
             text = "overweight, be careful, covid mostly affect obese people."
+            container.style.background = "linear-gradient(90deg, rgba(248,255,148,1) 0%, rgba(241,255,44,1) 62%, rgba(242,255,0,1) 100%)"
         } else if (imc >= 29.9 && imc < 34.9) {
             text = "obesity, watch out, covid can affect you a lot."
+            container.style.background = "linear-gradient(90deg, rgba(255,223,148,1) 0%, rgba(255,191,44,1) 60%, rgba(255,199,0,1) 100%)"
         } else if (imc >= 34.9 && imc < 39.9) {
             result.style.fontSize = "0.9em"
             text = "morbid obesity. Go to the doctor or covid could be dangerous"
+            container.style.background = "linear-gradient(90deg, rgba(255,167,89,1) 0%, rgba(255,120,44,1) 41%, rgba(255,101,0,1) 100%)"
         } else if (imc >= 39.9) {
             result.style.fontSize = "0.9em"
             text = "extreme obesity. Must visit the doctor or covid will kill you💀"
+            container.style.background = "linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(253,100,48,1) 48%, rgba(252,132,69,1) 100%)"
         }
 
         result.style.display = "flex"
