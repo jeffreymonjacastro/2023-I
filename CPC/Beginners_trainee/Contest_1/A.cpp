04d3ed825e608fc832698b13df6f095b54417488
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-14 18:15:37 -0500

    new

--- CPC/Beginners_trainee/Contest_1/A.cpp
+++ CPC/Beginners_trainee/Contest_1/A.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 
-void recursion(int &n){
-    std::cout<<n<<" ";
-    if(n == 1)
-        return;
-    else {
-        if (n%2 == 0) {
-            n /= 2;
-            recursion(n);
-        } else {
-            n = n * 3 +1;
-            recursion(n);
-        }
-    }
-}
-
 int main(){
     int n; std::cin>>n;
 
-    recursion(n);
+    while(n != 1){
+        if (n%2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 +1;
+        }
+
+        std::cout<<n<<" ";
+    }
 
     return 0;
 }
