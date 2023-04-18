b92a519fb0442933d3a75055a45bd720ddb78150
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-17 23:12:02 -0500

    D done

--- CPC/Beginners_trainee/Contest_1/D.cpp
+++ CPC/Beginners_trainee/Contest_1/D.cpp
@@ -1,3 +1,42 @@
 // Number Spiral
 
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+    ll t, x, y, n; cin>>t;
+
+    vector<pair<ll,ll>> nums(t);
+
+    for (int i = 0; i < t; ++i) {
+        cin >> x >> y;
+
+        nums[i] = {x, y};
+    }
+
+    for(auto i:nums){
+        n = max(i.first, i.second);
+
+        if (i.first == i.second) {
+            cout<<n*n-n+1<<endl;
+        } else if (n%2 == 0) {
+            if (i.first > i.second) {
+                cout << n * n - n + 1 + (i.first - i.second) << endl;
+            } else {
+                cout << n * n - n + 1 - (i.second - i.first) << endl;
+            }
+        } else {
+            if (i.first > i.second){
+                cout<<n * n - n + 1 - (i.first - i.second) << endl;
+            } else {
+                cout << n * n - n + 1 + (i.second - i.first) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
