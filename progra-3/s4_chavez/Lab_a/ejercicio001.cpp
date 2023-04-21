58abd541ae288dbff1c03e808adbbfa629c46777
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-20 23:33:57 -0500

    NOTHING

---
+++ progra-3/s4_chavez/Lab_a/ejercicio001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+T suma(T a, T b){
+    return a + b;
+}
+
+template<class T1, class T2>
+char suma(T1 a, T2 b){
+    return a + b;
+}
+
+int main(){
+    cout << suma(1,2) << endl;
+    cout << suma('A',1) << endl;
+    cout << suma(1,'A') << endl;
+    cout << suma('a','a') << endl;
+
+    return 0;
+}
