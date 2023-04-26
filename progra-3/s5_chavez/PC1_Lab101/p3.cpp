934dad6228eefcc009e884a6109ff16972c434e9
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-04-26 14:35:33 -0500

    No me sale

--- progra-3/s5_chavez/PC1_Lab101/p3.cpp
+++ progra-3/s5_chavez/PC1_Lab101/p3.cpp
@@ -2,23 +2,22 @@
 
 using namespace std;
 
-//template<typename T>
-//bool check_sum(T n, T m){
-//    if (n + m == 10)
-//        return true;
-//    return false;
-//}
+    //template<typename T>
+    //bool check_sum(T n, T m){
+    //    if (n + m == 10)
+    //        return true;
+    //    return false;
+    //}
 
 template<typename... Ts>
 bool check_sum(Ts... args){
-    (([&](auto arg1){cout<<arg1<<endl;}(args)),...);
-
+    return ((args + args == 10) || ...);
 }
 
 int main(){
     cout << check_sum (1 ,2 ,5 ,6 ,7 ,2 ,1 ,3) << endl ;
-//    cout << check_sum (1 ,2 ,5 ,6 ,7 ,2 ,1 ,1) << endl ;
-//    cout << check_sum (1 ,2 ,3 ,6 ,6 ,2 ,5 ,5) << endl ;
+    cout << check_sum (1 ,2 ,5 ,6 ,7 ,2 ,1 ,1) << endl ;
+    cout << check_sum (1 ,2 ,3 ,6 ,6 ,2 ,5 ,5) << endl ;
 
     return 0;
 }
