6dc3b829f42d0043d64a13df4638315f9c4f5ade
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-05-09 18:43:39 -0500

    Tarea 3

--- P3/s6_chavez/tarea-1c/tarea/main.cpp
+++ P3/s6_chavez/tarea-1c/tarea/main.cpp
@@ -8,14 +8,15 @@
 #include "foo.h"
 
 // Puede validar su codigo con la siguiente funcion main
-int main(){
+int main() {
 //    string richtext;
+//
 //    ifstream doc(R"(C:\Users\Usuario\Github\2023-I\P3\s6_chavez\tarea-1c\tarea\document.html)");
 //
-//    if(doc.is_open()){
+//    if (doc.is_open()) {
 //        string linea;
 //
-//        while(getline(doc, linea))
+//        while (getline(doc, linea))
 //            richtext += linea + "\n";
 //
 //        doc.close();
@@ -24,12 +25,15 @@ int main(){
 //        cout << "No se pudo abrir el archivo" << endl;
 //    }
 
-    // todo: Para quitar todo lo que está arriba del <head>
+//    cout << richtext << endl;
+
+// todo: Para quitar todo lo que está arriba del <head>
 
     string richtext = "<head>Holaaa</head>";
 
-    bool pos1 = false;
-    bool pos2 = false;
+    string newtext;
+
+    bool pos1 = false, pos2 = false;
 
     for (int i = 0; i < richtext.size(); ++i) {
         int n, m;
@@ -37,24 +41,20 @@ int main(){
         if (richtext[i] == '<'){
             n = i;
             pos1 = true;
-        } else if (richtext[i] == '>'){
+        } else if (richtext[i] == '>' && pos1){
             m = i;
             pos2 = true;
         }
 
-        if (pos1 && pos2) {
-            cout << "Nice";
+        if (!pos1 && !pos2) {
+
+            pos1 = false;
+            pos2 = false;
+
         }
-
-
-
-
-
-
-        
     }
 
-//    cout << newtext << endl;
+    cout << richtext << endl;
 
 // todo: codigo para leer solamente alfanuméricos
 
@@ -74,8 +74,6 @@ int main(){
 //            iter++;
 //        }
 //    }
-
-
-
+//
     return 0;
 }
