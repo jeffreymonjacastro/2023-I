ef99c118dd8a43b01b5c9725c15779c525dbd65f
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-05-31 21:38:15 -0500

    New

---
+++ P3/s10/PC2_practice/Repasito_Contenedores.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+/* TEMAS PC2:
+    * Callables y Librería Algorithm
+    -> Contenedores e Iteradores
+    * Complejidad Algoritmica e Invariante de Bucle
+    * Programación Concurrente
+*/
+
+/*
+    SEGUNDO TEMA: CONTENEDORES E ITERADORES
+    -> Contenedores Secuenciales
+        - Almacena los elementos en una secuencia
+        - Almacena elementos del mismo tipo
+        - Se puede controlar el orden (indice) en el que se accede e inserta los elementos
+    * Tipos secuenciales:
+        - Vector: elementos adjuntos (accesos por subindices, acceso aleatorio)
+        - Array: elementos adjuntos (accesos por subindices, acceso aleatorio)
+        - List: elementos enlazados (accesos por iteradores, acceso secuencial)
+
+    -> Contenedores Asociativos
+*/
+
+int main(){
+
+    return 0;
+}
