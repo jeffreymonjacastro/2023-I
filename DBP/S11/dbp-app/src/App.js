1525fa97c12a2d5d63b2077248df63a6e4989e15
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-06-07 17:04:33 -0500

    update

--- DBP/S11/dbp-app/src/App.js
+++ DBP/S11/dbp-app/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Funciones flecha
+// const MyApp = () => {}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2> Hello World </h2>
+        <h3> Jeffrey WebApp </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
