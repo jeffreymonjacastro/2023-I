12bb0768409f725768f618711ecba5d4168450fc
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-06-08 16:31:37 -0500

    S11

--- DBP/S11/dbp-app/src/App.js
+++ DBP/S11/dbp-app/src/App.js
@@ -1,30 +1,41 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Index } from './components/Calculadora'
 
 // Funciones flecha
 // const MyApp = () => {}
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2> Hello World </h2>
-        <h3> Jeffrey WebApp </h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
+    
+      <Index />
+
     </div>
-  );
+  )
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <h2> Hello World </h2>
+  //       <h3> XD WebApp </h3>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ DBP/S11/dbp-app/src/components/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Boton } from './Boton'
+
+export const Index = () => {
+    return (
+        <div>
+            <h2>
+            calculadora
+            </h2>
+
+            <Boton />
+        </div>
+    );
+}
