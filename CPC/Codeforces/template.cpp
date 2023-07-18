fa93fba4ab857928e543930dd4be30e839cac38e
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-07-17 22:29:01 -0500

    CPC

--- CPC/Codeforces/template.cpp
+++ CPC/Codeforces/template.cpp
@@ -8,13 +8,7 @@ typedef long double lld;
 
 
 void solve(){
-  string s; 
-  cin>>s;
-  
-  if(s.length() <= 10)
-    cout<<s<<endl;
-  else
-    cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+
 }
 
 int main(){
